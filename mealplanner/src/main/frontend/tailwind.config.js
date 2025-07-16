5b6af6b2aa7558c3386b4c9159d1c92e07719aa4
Osman Goni <osmancse32@gmail.com>
2025-07-16 19:54:26 +0600

    ui changed based on feedback

--- mealplanner/src/main/frontend/tailwind.config.js
+++ mealplanner/src/main/frontend/tailwind.config.js
@@ -8,14 +8,29 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#ff7300',
+        primary: '#FFA544',
+        customTomato: '#550000',
         secondary: '#F8F9FA',
         success: '#28A945',
         warning: '#F59E0B',
         danger: '#DC2626',
         gray: { light: '#CED4DA' }
+      },
+      fontFamily: {
+        dmSerifDisplay: ['"DM Serif Display"', 'serif'],
       }
     },
   },
+  safelist: [
+    'text-orange-100',
+    'text-orange-200',
+    'text-orange-300',
+    'text-orange-400',
+    'text-orange-500',
+    'text-orange-600',
+    'text-orange-700',
+    'text-orange-800',
+    'text-orange-900',
+  ],
   plugins: [],
 } 
